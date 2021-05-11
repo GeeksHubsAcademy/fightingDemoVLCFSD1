473bd9030072795ad437cd1c2ecc176111531178
Dave86dev <davidochando1986@gmail.com>
2021-05-11 10:50:39 +0200

    Versus screen implementada y primer hit de p1 a p2

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@
 let team1 = [];
 let team2 = [];
 
+let p1 = "";
+let p2 = "";
+
 
 const cambiaFase = (destino) => {
     
-    let arrFase = ["fase1","fase2","fase3"];
+    let arrFase = ["fase1","fase2","fase3","fase4"];
 
     arrFase = arrFase.filter(val => !destino.includes(val));
 
@@ -32,7 +35,12 @@ const chooseFighter = (fighter) => {
             if(team2.length == 2){
                 console.log("ESTE ES EL TEAM1 ", team1);
                 console.log("ESTE ES EL TEAM2 ", team2);
+                llenaEquipos();
                 cambiaFase("fase3");
+
+                setTimeout(() => {
+                    cambiaFase("fase4");
+                }, 5000);
             }
             
         }
@@ -43,4 +51,36 @@ const chooseFighter = (fighter) => {
         
     }
         
+}
+
+const llenaEquipos = () => {
+    let equipos = document.getElementById("equipos");
+
+    equipos.innerHTML = `
+    <div class="teamCharacters">
+        <div><img class="picFighter" src="img/${team1[0].nombre}.png" alt="luchador1"></div>
+        <div><img class="picFighter" src="img/${team1[1].nombre}.png" alt="luchador3"></div>
+    </div>
+    <div class="fightPanel"><img class="fotoVersus" src="img/fight.png" alt="lucha"></div>
+    <div class="teamCharacters">
+        <div><img class="picFighter" src="img/${team2[0].nombre}.png" alt="luchador2"></div>
+        <div><img class="picFighter" src="img/${team2[1].nombre}.png" alt="luchador4"></div>
+    </div>
+    `;
+}
+
+const fighting = () => {
+
+    p1 = team1[0];
+    p2 = team2[0];
+
+    console.log("empieza la lucha");
+
+    p1.hit(p2);
+
+    console.log(p1.vida,p2.vida);
+    // do{
+
+    // }while();
+
 }
